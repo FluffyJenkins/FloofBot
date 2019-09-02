9b46d068ae9e93ac63fca59cf10af5e971fcb623
Fluffy Jenkins <richardjrn.w@gmail.com>
2019-09-02 17:43:57 +0100

    I am an idiot sandwich uwu

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const botToken = require("./token");
+const botToken = require("./token").token;
 
 const COMMAND_TRIGGER = ">";
 
